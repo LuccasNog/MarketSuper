d72678039805b9c0ab56417606000be1c1484905
lucasgo1099@gmail.com <you@example.com>
2022-04-26 21:24:51 -0300

    initial project pages

---
+++ lib/screens/initial.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class InitialPage extends StatelessWidget {
+  const InitialPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.redAccent,
+        body: meuLayoutWidget(),
+      ),
+    );
+  }
+}
+
+Widget meuLayoutWidget() {
+  return Padding(
+    padding: const EdgeInsets.all(12.0),
+    child: Center(
+        child: Image.asset(
+      'images/shopping-cart.png',
+      width: 100,
+      height: 120,
+    )),
+  );
+}
