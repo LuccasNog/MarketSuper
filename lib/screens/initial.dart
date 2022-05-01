ffe49829ace4e11b447671fa1f3e61ff5a47c4dc
lucasgo1099@gmail.com <you@example.com>
2022-04-30 22:08:24 -0300

    add screen lottie in page login

--- lib/screens/initial.dart
+++ lib/screens/initial.dart
@@ -1,32 +1,75 @@
+import 'package:app/screens/login.dart';
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
 
 class InitialPage extends StatelessWidget {
   const InitialPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.redAccent,
-        body: meuLayoutWidget(),
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Center(
+        child: Lottie.network(
+            'https://assets9.lottiefiles.com/packages/lf20_6sxyjyjj.json'),
       ),
     );
   }
 }
 
+/*
+
+Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              /*
+              Image.asset(
+                'images/icon_carrin.png',
+                width: 100,
+                height: 120,
+              ),
+              */
+              
+              ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => LoginPage()),
+                    );
+                  },
+                  child: Text('Boas compra'))
+            ],
+          ),
+        ),
+      ),
+   */   
+
+/*
 Widget meuLayoutWidget() {
   return Padding(
     padding: const EdgeInsets.all(12.0),
     child: Center(
-        child: GestureDetector(
-      child: Image.asset(
-        'images/shopping-cart.png',
-        width: 100,
-        height: 120,
+      child: Column(
+        children: [
+          Image.asset(
+            'images/icon_carrin.png',
+            width: 100,
+            height: 120,
+          ),
+          ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => LoginPage()),
+                );
+              },
+              child: Text('Boas compra'))
+        ],
       ),
-      onTap: () {
-        print('Imagem foi clicada');
-      },
-    )),
+    ),
   );
 }
+*/
