46456125e6ad872fa39db5b9098396a7bfb30259
lucasgo1099@gmail.com <you@example.com>
2022-05-14 15:34:28 -0300

    add correction

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -10,16 +10,92 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   //final TextEditingController email = TextEditingController();
   //final TextEditingController password = TextEditingController();
+  final TextEditingController email = TextEditingController();
+  final TextEditingController password = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
-      body: page(),
+      body: Padding(
+        padding: const EdgeInsets.all(19.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const SizedBox(
+              height: 30,
+              width: 90,
+            ),
+            Image.asset(
+              'images/icons_avatar.png',
+              width: 100,
+              height: 120,
+            ),
+            const Text(
+              'Acesse sua conta',
+            ),
+            const SizedBox(
+              height: 30,
+              width: 90,
+            ),
+            TextFormField(
+              controller: email,
+              decoration: InputDecoration(
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: const BorderSide(color: Colors.red, width: 2.0),
+                    borderRadius: BorderRadius.circular(29.0),
+                  ),
+                  hintText: 'name@example.com',
+                  prefixIconColor: Colors.red),
+              keyboardType: TextInputType.emailAddress,
+              obscureText: false,
+            ),
+            SizedBox(
+              height: 30,
+              width: 90,
+            ),
+            TextFormField(
+              controller: password,
+              decoration: InputDecoration(
+                focusedBorder: OutlineInputBorder(
+                  borderSide: const BorderSide(color: Colors.red, width: 2.0),
+                  borderRadius: BorderRadius.circular(25.0),
+                ),
+                iconColor: Colors.red,
+                labelText: 'Informe sua senha',
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(30),
+                ),
+              ),
+              obscureText: true,
+            ),
+            SizedBox(
+              height: 30,
+              width: 90,
+            ),
+            TextButton(
+              onPressed: () {
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => LoginPage(),
+                  ),
+                );
+              },
+              child: const Text(
+                'Entrar',
+                style: TextStyle(color: Colors.red),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
 
+/*
 Widget page() {
   final TextEditingController email = TextEditingController();
   final TextEditingController password = TextEditingController();
@@ -40,7 +116,6 @@ Widget page() {
         ),
         const Text(
           'Acesse sua conta',
-          style: TextStyle(color: Colors.blueAccent),
         ),
         const SizedBox(
           height: 30,
@@ -49,13 +124,12 @@ Widget page() {
         TextFormField(
           controller: email,
           decoration: InputDecoration(
-            labelText: 'Informe seu e-mail',
-            hintText: 'name@example.com',
-            border: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(30),
-            ),
-            prefixIcon: const Icon(Icons.email),
-          ),
+              focusedBorder: OutlineInputBorder(
+                borderSide: const BorderSide(color: Colors.red, width: 2.0),
+                borderRadius: BorderRadius.circular(29.0),
+              ),
+              hintText: 'name@example.com',
+              prefixIconColor: Colors.red),
           keyboardType: TextInputType.emailAddress,
           obscureText: false,
         ),
@@ -66,37 +140,38 @@ Widget page() {
         TextFormField(
           controller: password,
           decoration: InputDecoration(
-              iconColor: Colors.red,
-              labelText: 'Informe sua senha',
-              border: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(30),
-              ),
-              prefixIcon: Icon(Icons.security)),
+            focusedBorder: OutlineInputBorder(
+              borderSide: const BorderSide(color: Colors.red, width: 2.0),
+              borderRadius: BorderRadius.circular(25.0),
+            ),
+            iconColor: Colors.red,
+            labelText: 'Informe sua senha',
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(30),
+            ),
+          ),
           obscureText: true,
         ),
         SizedBox(
           height: 30,
           width: 90,
         ),
-        ElevatedButton(
-          style: ElevatedButton.styleFrom(
-            primary: Colors.blue,
-            onPrimary: Colors.white,
-            shadowColor: Colors.greenAccent,
-            elevation: 2,
-            shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(32.0)),
-            minimumSize: const Size(100, 40), //////// HERE
-          ),
+        TextButton(
           onPressed: () {
-            print('$email.value, $password.value');
+            Navigator.pushReplacement(
+              context,
+              MaterialPageRoute(
+                builder: (context) => LoginPage(),
+              ),
+            );
           },
-          child: Text('Entrar'),
-        )
+          child: const Text('Entrar'),
+        ),
       ],
     ),
   );
 }
+*/
 
 Widget CircleUser() {
   return CircleAvatar(
--- lib/screens/register.dart
+++ lib/screens/register.dart
@@ -1,1 +1,177 @@
+import 'package:app/screens/login.dart';
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+import 'package:email_validator/email_validator.dart';
+
+class RegisterPage extends StatefulWidget {
+  //const RegisterPage({Key? key, required this.title}) : super(key: key);
+  //final String title;
+
+  @override
+  State<RegisterPage> createState() => _RegisterPageState();
+}
+
+class _RegisterPageState extends State<RegisterPage> {
+  final _formKey = GlobalKey<FormState>();
+  var rememberValue = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      //backgroundColor: Theme.of(context).colorScheme.background,
+      backgroundColor: Colors.black,
+      body: Container(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'Cadastre',
+              style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 40,
+                  color: Colors.white),
+            ),
+            const SizedBox(
+              height: 60,
+            ),
+            Form(
+              key: _formKey,
+              child: Column(
+                children: [
+                  Row(
+                    children: [
+                      Expanded(
+                        child: TextFormField(
+                          validator: (value) => EmailValidator.validate(value!)
+                              ? null
+                              : "Por favor adicione um e-mail válido",
+                          maxLines: 1,
+                          decoration: InputDecoration(
+                            hintText: 'Nome',
+                            focusedBorder: OutlineInputBorder(
+                              borderSide: const BorderSide(
+                                  color: Colors.red, width: 2.0),
+                              borderRadius: BorderRadius.circular(10.0),
+                            ),
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(
+                        width: 20,
+                      ),
+                      Expanded(
+                        child: TextFormField(
+                          validator: (value) => EmailValidator.validate(value!)
+                              ? null
+                              : 'Por favor, digite um e-mail',
+                          maxLines: 1,
+                          decoration: InputDecoration(
+                            hintText: 'Sobrenome',
+                            focusedBorder: OutlineInputBorder(
+                              borderSide: const BorderSide(
+                                  color: Colors.red, width: 2.0),
+                              borderRadius: BorderRadius.circular(10.0),
+                            ),
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  TextFormField(
+                    validator: (value) => EmailValidator.validate(value!)
+                        ? null
+                        : "Please enter a valid email",
+                    maxLines: 1,
+                    decoration: InputDecoration(
+                      hintText: 'Informe seu e-mail',
+                      focusedBorder: OutlineInputBorder(
+                        borderSide:
+                            const BorderSide(color: Colors.red, width: 2.0),
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  TextFormField(
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Por favor digite sua senha';
+                      }
+                      return null;
+                    },
+                    maxLines: 1,
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      focusedBorder: OutlineInputBorder(
+                        borderSide:
+                            const BorderSide(color: Colors.red, width: 2.0),
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                      hintText: 'Informe sua senha',
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  TextButton(
+                    onPressed: () {
+                      if (_formKey.currentState!.validate()) {}
+                    },
+                    child: const Text(
+                      'Registrar',
+                      style: TextStyle(color: Colors.red),
+                    ),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Text(
+                        'Tem Cadastro?',
+                      ),
+                      TextButton(
+                        onPressed: () {
+                          Navigator.pushReplacement(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => LoginPage(),
+                            ),
+                          );
+                        },
+                        child: const Text(
+                          'Entrar',
+                          style: TextStyle(color: Colors.red),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
